5ab19f87d2cef4ca73daf7a8fb4d2f81a3168604
Mehrdad Rahmani <mehidad@outlook.com>
2022-06-14 13:37:29 +0430

    init

---
+++ EntitiesTests/Owin/UserConfigurationTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entities.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Owin.Tests
+{
+    [TestClass()]
+    public class UserConfigurationTests
+    {
+        [TestMethod()]
+        public void ConfigureTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
